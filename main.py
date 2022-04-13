606af128c79fd55478c37a8aaf4ec9c57362e70e
salfk23 <codetbared@gmail.com>
2022-04-14 05:58:26 +0800

    GameUI class blueprint

--- main.py
+++ main.py
@@ -4,30 +4,46 @@ import pygame
 
 WIDTH, HEIGHT = 800, 600
 
+
+class Colors:
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+    PURPLE = (255, 0, 255)
+    CYAN = (0, 255, 255)
+
+
 class GameUI:
-  def __init__(self):
-      self.entities = []
-      pygame.display.set_mode((WIDTH, HEIGHT))
+    def __init__(self):
+        self.entities = []
+        self.window = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("2D RPG Game")
 
-  def draw(self, screen):
-      for entity in self.entities:
-          entity.draw(screen)
+    def draw(self):
+        self.window.fill(Colors.WHITE)
+        pygame.display.update()
 
+    def run(self):
+        loop = True
+        clock = pygame.time.Clock()
 
+        while loop:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    loop = False
+            self.draw()
 
 
 def main():
-  print("Running!")
-  ui = GameUI()
-  # ui.run()
-  loop = True
+    print("Running!")
+    ui = GameUI()
+    ui.run()
+    pygame.quit()
 
-  while loop:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        loop = False
-
-  pygame.quit()
 
 if __name__ == '__main__':
     main()
