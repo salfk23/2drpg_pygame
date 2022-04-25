e6b834b641aa6aa3e1e692d446dc8aac28e283e5
salfk23 <codetbared@gmail.com>
2022-04-25 21:34:06 +0800

    Added State for entities

---
+++ game_engine/entities/state.py
@@ -0,0 +1,12 @@
+
+class Hurtable:
+  def __init__(self, health: int, max_health: int):
+    self.health = health
+    self.max_health = max_health
+
+class HurtBox:
+  def __init__(self, x1: int, y1: int, x2: int, y2: int):
+    self.x1 = x1
+    self.y1 = y1
+    self.x2 = x2
+    self.y2 = y2
