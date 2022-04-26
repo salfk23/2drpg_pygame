c433e6f7e5f5c0c77a1a4d5f68e9f49ba5d1d8f6
salfk23 <codetbared@gmail.com>
2022-04-27 00:51:51 +0800

    Revamped Character Entity

--- game_engine/entities/characters.py
+++ game_engine/entities/characters.py
@@ -10,12 +10,14 @@ from game_engine.helpers import Colors, Size2D
 
 
 class Healthbar(Entity):
-  def __init__(self, size: Size2D):
+  def __init__(self, size: pygame.Vector2):
       super().__init__((0,0), size)
       self.object = pygame.Surface(size)
       self.object.fill(Colors.RED)
       self._health = 0
       self.max_health = 100
+      self.size = size
+      self.on_health_change()
       # Create a rectangle with color green
       # rect = pygame.Rect(0, 0, size.x, size.y)
       # rect.center = self.object.get_rect().center
@@ -29,20 +31,20 @@ class Healthbar(Entity):
   @health.setter
   def health(self, health: int):
     if health != self._health:
+      self._health = health
       self.on_health_change()
-    self._health = health
 
   def on_health_change(self):
-    rect = pygame.Rect(0, 0, self.size.x, self.size.y)
-    rect.center = self.object.get_rect().center
-    rect.width = self.health * self.size.x / self.max_health
-    pygame.Surface.fill(self.object, Colors.GREEN, rect)
+    pygame.draw.rect(self.object, Colors.YELLOW, (0, 0, self.size.x, self.size.y))
+    rect = pygame.Rect(0, 0, self.size.x* self.health / self.max_health, self.size.y)
+    pygame.draw.rect(self.object, Colors.GREEN, rect)
+
 
 
 class Character(ControllableEntity, Hurtable, AffectedByGravity, Solid):
   def __init__(self, position: pygame.Vector2, size: Size2D, speed: int, jump_power: int):
       ControllableEntity.__init__(self, position, size, speed, jump_power)
-      Hurtable.__init__(self, 100, 100)
+      Hurtable.__init__(self, 100, 150)
 
   @property
   def health(self):
@@ -58,9 +60,10 @@ class Character(ControllableEntity, Hurtable, AffectedByGravity, Solid):
 class Enemy(Character):
   def __init__(self, position: pygame.Vector2, size: Size2D, speed: int, jump_power: int):
       Character.__init__(self, position, size, speed, jump_power)
-      self.health_bar = Healthbar((50, 10))
+      self.health_bar = Healthbar(pygame.Vector2(50, 10))
       self.linked.append(self.health_bar)
       self.on_position_change()
+      self.on_health_change()
   def on_position_change(self):
       super().on_position_change()
       health_bar_rect = self.health_bar.rect
@@ -69,6 +72,6 @@ class Enemy(Character):
       self.health_bar.position = health_bar_rect.topleft
   def on_health_change(self):
       super().on_health_change()
-      self.health_bar.health = self.health
       self.health_bar.max_health = self.max_health
+      self.health_bar.health = self.health
 
