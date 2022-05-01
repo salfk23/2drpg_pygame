3bb2432e4818f6b496cdf3e317b7c1e27a2a1dce
salfk23 <codetbared@gmail.com>
2022-05-01 09:20:35 +0800

    Fixed Enemy weapon placement

--- game_engine/entities/characters.py
+++ game_engine/entities/characters.py
@@ -1,10 +1,7 @@
-
-from typing import Sequence
 import pygame
-from game_engine.entities import weapon
 from game_engine.entities.dynamic import AffectedByGravity, ControllableEntity
 
-from game_engine.entities.entity import NORMAL_SIZE_ENTITY, BiDirectionalEntity, Entity
+from game_engine.entities.entity import BiDirectionalEntity, Entity
 from game_engine.entities.particles import ExplosionParticle
 from game_engine.entities.state import Hurtable, Solid
 from game_engine.entities.weapon import Weapon
@@ -13,8 +10,9 @@ from game_engine.helpers import Colors, Size2D, load_image
 
 
 class Healthbar(Entity):
-  def __init__(self, size: pygame.Vector2):
+  def __init__(self, size: Size2D):
       super().__init__((0,0), size)
+      self.name = "Healthbar"
       self.sprite = pygame.Surface(size)
       self.sprite.fill(Colors.RED)
       self._health = 0
@@ -31,8 +29,8 @@ class Healthbar(Entity):
     self.on_health_change()
 
   def on_health_change(self):
-    pygame.draw.rect(self.sprite, Colors.YELLOW, (0, 0, self.size.x, self.size.y))
-    rect = pygame.Rect(0, 0, self.size.x* self._health / self.max_health, self.size.y)
+    pygame.draw.rect(self.sprite, Colors.RED, (0, 0, self.size[0], self.size[1]))
+    rect = pygame.Rect(0, 0, self.size[0]* self._health / self.max_health, self.size[1])
     pygame.draw.rect(self.sprite, Colors.GREEN, rect)
 
 
@@ -64,36 +62,38 @@ class Character(ControllableEntity, BiDirectionalEntity, Hurtable, AffectedByGra
 stick_image = load_image("assets/stick.png")
 class Enemy(Character):
   def __init__(self, image:pygame.Surface, position: pygame.Vector2, size: Size2D, speed: int, jump_power: int):
+      self.weapon = None
       Character.__init__(self, image, position, size, speed, jump_power)
-      self.health_bar = Healthbar(pygame.Vector2(50, 10))
-      # stick = Weapon(stick_image, (10, 50), 1, 1)
-      # stick.fill(Colors.RED)
-      # stick = pygame.transform.rotate(stick_image, -10)
+      self.health_bar = Healthbar((50, 10))
       IMAGE = pygame.Surface((20, 50), pygame.SRCALPHA)
-      IMAGE.fill(Colors.YELLOW)
-      pygame.draw.polygon(IMAGE, pygame.Color('red'), ( (0, 0), (20, 0),(6, 50)))
-      IMAGE = pygame.transform.rotate(IMAGE, 180)
+      IMAGE.fill(Colors.CYAN)
+      pygame.draw.polygon(IMAGE, pygame.Color('red'), ( (5, 0), (20, 50),(5, 50)))
+      self.weapon = Weapon(pygame.Vector2(self.rect.right,self.rect.centery),IMAGE, (10, 50), 30, 10)
+      self.weapon.owner = self
       self.weapon = Weapon(pygame.Vector2(self.rect.right,self.rect.centery),IMAGE, (10, 50), 10, 1)
+      self.weapon.owner = self
       self.linked.append(self.health_bar)
       self.linked.append(self.weapon)
       self.on_position_change()
       self.on_health_change()
+  def on_name_change(self):
+      self.health_bar.name = self.name+"_Healthbar"
+      self.weapon.name = f"{self.name}_Weapon_{self.weapon.name}"
+
+  def on_direction_change(self):
+      super().on_direction_change()
+      if self.weapon is not None:
+        self.weapon.direction = self.direction
   def on_position_change(self):
       super().on_position_change()
       health_bar_rect = self.health_bar.rect
       health_bar_rect.center = self.rect.center
       health_bar_rect.top = self.rect.top - health_bar_rect.height - 10
-      weapon_rect = self.weapon.rect
-      weapon_rect.bottom = self.rect.centery
       if self.direction:
-        weapon_rect.right = self.rect.right
-        self.weapon.anchor = pygame.Vector2(weapon_rect.right, weapon_rect.centery)
+        self.weapon.anchor = pygame.Vector2(self.rect.right, self.rect.centery)
       else:
-        weapon_rect.left = self.rect.left - weapon_rect.width
-        self.weapon.anchor = pygame.Vector2(weapon_rect.left, weapon_rect.centery)
-
-      self.health_bar.position = health_bar_rect.topleft
-      self.weapon.position = weapon_rect.topleft
+        self.weapon.anchor = pygame.Vector2(self.rect.left, self.rect.centery)
+      self.health_bar.position = pygame.Vector2(health_bar_rect.topleft)
   def on_health_change(self):
       super().on_health_change()
       self.health_bar.max_health = self.max_health
@@ -103,20 +103,19 @@ class Enemy(Character):
         center = self.rect.center
         ExplosionParticle.create_particles(pygame.Vector2(center), 100, Colors.RED, (2, 10), (0.1, 3))
         self.remove = True
+  def update(self):
+      super().update()
+      # Get distance to player
 
 
 class Player(Character):
   def __init__(self, image:pygame.Surface, position: pygame.Vector2, size: Size2D, speed: int, jump_power: int):
       self.weapon = None
       Character.__init__(self, image, position, size, speed, jump_power)
-      # stick = Weapon(stick_image, (10, 50), 1, 1)
-      # stick.fill(Colors.RED)
-      # stick = pygame.transform.rotate(stick_image, -10)
       IMAGE = pygame.Surface((20, 50), pygame.SRCALPHA)
       IMAGE.fill(Colors.CYAN)
       pygame.draw.polygon(IMAGE, pygame.Color('red'), ( (5, 0), (20, 50),(5, 50)))
       self.weapon = Weapon(pygame.Vector2(self.rect.right,self.rect.centery),IMAGE, (10, 50), 30, 10)
-      self.weapon.offset = pygame.Vector2(10, -25)
       self.weapon.owner = self
       self.linked.append(self.weapon)
       self.on_position_change()
