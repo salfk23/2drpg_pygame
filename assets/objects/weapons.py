c6ee3cd9c0f97d1164433ca51e217c4fd4b6cdc0
salfk23 <codetbared@gmail.com>
2022-05-03 04:49:34 +0800

    Added game weapons

--- assets/objects/weapons.py
+++ assets/objects/weapons.py
@@ -1,16 +1,36 @@
 import pygame
-from game_engine.entities.weapon import Melee, Weapon
+from game_engine.entities.weapon import Melee
 from game_engine.helpers import Colors
+from assets.images import battleaxe_image, mace_image,silversword_image, trident_image, warhammer_image
 
 SampleWeaponSprite = pygame.Surface((20, 50), pygame.SRCALPHA)
 SampleWeaponSprite.fill(Colors.CYAN)
 pygame.draw.polygon(SampleWeaponSprite, pygame.Color('red'), ( (5, 0), (20, 50),(5, 50)))
 
-sample_weapon = Melee(pygame.Vector2(0,0),SampleWeaponSprite, (10, 50), 30, 10)
+sample_weapon = Melee(pygame.Vector2(0,0),SampleWeaponSprite, 30, 10)
+
+battleaxe = Melee(
+  pygame.Vector2(0,0),
+  pygame.transform.scale(battleaxe_image, (50, 75)),
+  75, 7)
+mace = Melee(
+  pygame.Vector2(0,0),
+  pygame.transform.scale(mace_image, (40, 52)),
+  30, 8)
+
+silversword = Melee(
+  pygame.Vector2(0,0),
+  pygame.transform.scale(silversword_image, (45, 62)),
+  40, 9)
 
 
-SampleWeaponSprite = pygame.Surface((20, 50), pygame.SRCALPHA)
-SampleWeaponSprite.fill(Colors.YELLOW)
-pygame.draw.polygon(SampleWeaponSprite, pygame.Color('red'), ( (5, 0), (20, 50),(5, 50)))
+trident = Melee(
+  pygame.Vector2(0,0),
+  pygame.transform.scale(trident_image, (50, 75)),
+  95, 5)
 
-sample_weapon2 = Melee(pygame.Vector2(0,0),SampleWeaponSprite, (10, 50), 30, 10)
+
+warhammer = Melee(
+  pygame.Vector2(0,0),
+  pygame.transform.scale(warhammer_image, (60, 90)),
+  120, 3)
